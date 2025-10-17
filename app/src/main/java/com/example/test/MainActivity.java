717b88e24482acf0e7adcecb74cc284096dc63ec
Jakub224 <jakub3szczesniak@gmail.com>
2025-10-17 13:47:56 +0200

    Utworznie nowej zmiennej typu Button w MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button nowyButton;
     }
 }
